817479d6ca27de8062ab6ed5267810f4f84973b1
Scotturbina <kevin.urbinamoreira@hotmail.com>
2017-01-22 16:09:15 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,3 +3,9 @@ activity_Labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 activity_Labels[,2] <- as.character(activity_Labels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
+
+features_Required <- grep(".*mean.*|.*std.*", features[,2])
+features_Required.names <- features[features_Required,2]
+features_Required.names = gsub('-mean', 'Mean', features_Required.names)
+features_Required.names = gsub('-std', 'Std', features_Required.names)
+features_Required.names <- gsub('[-()]', '', features_Required.names)
