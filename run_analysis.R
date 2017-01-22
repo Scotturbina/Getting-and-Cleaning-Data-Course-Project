e45a12dd8921caf2130d791d21a97f0ec4e2e55c
Scotturbina <kevin.urbinamoreira@hotmail.com>
2017-01-22 16:00:16 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
 ## Loading the activity labels and other features
 activity_Labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 activity_Labels[,2] <- as.character(activity_Labels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
