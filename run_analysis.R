51027d3c8910e683e075652af0b60697826e2e5d
Scotturbina <kevin.urbinamoreira@hotmail.com>
2017-01-22 16:10:14 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@ activity_Labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 activity_Labels[,2] <- as.character(activity_Labels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
-
+## Extracing mean and standard deviation data
 features_Required <- grep(".*mean.*|.*std.*", features[,2])
 features_Required.names <- features[features_Required,2]
 features_Required.names = gsub('-mean', 'Mean', features_Required.names)
